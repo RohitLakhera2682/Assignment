59c58301ad0bd72a4ef8d4934fd0e22f2e755437
Rohit Lakhera <rohitlakhera2682@gmail.com>
2024-11-30 21:00:58 +0530

    integrated_api

--- lib/actions/agents.ts
+++ lib/actions/agents.ts
@@ -13,14 +13,14 @@ export async function ListAgents() {
 }
 
   
-export async function CreateAgents() {
-    const agentResponse = await client.agent.create({
-      response_engine: { llm_id: 'llm_234sdertfsdsfsdf', type: 'retell-llm' },
-      voice_id: '11labs-Adrian',
-    });
+// export async function CreateAgents() {
+//     const agentResponse = await client.agent.create({
+//       response_engine: { llm_id: 'llm_234sdertfsdsfsdf', type: 'retell-llm' },
+//       voice_id: '11labs-Adrian',
+//     });
   
-    console.log(agentResponse.agent_id); 
-}
+//     console.log(agentResponse.agent_id); 
+// }
 
     // async function getAgents() {
     //   const agentResponse = await client.agent.retrieve('16b980523634a6dc504898cda492e939');
